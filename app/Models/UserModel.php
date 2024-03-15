3b7dd0003e584f8636e0f63b300f2ce0ad3800fe
akayanuarcandra <hazumiya.yc@gmail.com>
2024-03-15 14:16:51 +0700

    push 7 jobsheet4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
     use HasFactory;
-
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
@@ -18,4 +18,9 @@ class UserModel extends Model
         'nama',
         'password',
     ];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
