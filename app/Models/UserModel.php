f582752af61e9e67a7ecdee222ab4f602753e7f7
akayanuarcandra <hazumiya.yc@gmail.com>
2024-05-12 15:14:05 +0700

    jobsheet 9 done

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,25 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user';
-    public $timestamps = false;
     protected $primaryKey = 'user_id';
 
-    protected $fillable = [
-        'user_id',
-        'level_id',
-        'username',
-        'name',
-        'password',
-    ];
+    protected $fillable = ['level_id','username','nama','password'];
 
-    public function level()
+    public function level(): BelongsTo
     {
-        return $this->hasOne(LevelModel::class, 'level_id', 'level_id');
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function penjualan() {
+        return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
     }
 }
